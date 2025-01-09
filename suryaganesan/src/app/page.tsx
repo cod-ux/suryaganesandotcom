9bd060397948c6a9939be00f50c6e81b9b559931
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2025-01-09 20:55:26 +0000

    updated content

--- suryaganesan/src/app/page.tsx
+++ suryaganesan/src/app/page.tsx
@@ -9,6 +9,13 @@ export default function Home() {
           deeply passionate about <i>agentic ai</i> and its potential to solve
           hard problems. my goal is to work with similarly passionate teams
           tackling new challenges with software and become a better builder.
+          <br />
+          <br />
+          one of my favourite things to do before building new ideas is to judge
+          how important the problem i am solving is by having lots of
+          conversations wilth potential users. i am most excited about projects
+          that will solve painful problems for users. in my free time, i love
+          immersing in great stories - anime, video games, movies, etc.
         </p>
       </div>
     </div>
